b62ff0c09702138acc1764269ddf18cf3c0ff2b2
skyllo <skyllo.dev@gmail.com>
2016-11-19 12:22:05 +0000

    SAVEPOINT

--- config/webpack.base.babel.js
+++ config/webpack.base.babel.js
@@ -1,14 +1,8 @@
 import { resolve } from 'path';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 
 export default (env) => ({
-  entry: {
-    js: './index.js',
-    vendor: ['react', 'react-dom']
-  },
-
   output: {
     filename: '[name].bundle.js',
     path: resolve(__dirname, '../dist'),
@@ -23,14 +17,6 @@ export default (env) => ({
         test: /\.js$/,
         loaders: ['babel-loader'],
         exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: "style-loader",
-          loader: "css-loader"
-        }),
-        exclude: /node_modules/
       }
     ]
   },
@@ -41,35 +27,11 @@ export default (env) => ({
       template: 'index.html'
     }),
 
-    // assign modules and chunks shorter ids to lower file size
-    new webpack.optimize.OccurrenceOrderPlugin(),
-
-    // seperates out the vendor file into another chunk
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: Infinity,
-      filename: 'vendor.bundle.js'
-    }),
-
-    // extracts CSS from JS and puts into external file
-    new ExtractTextPlugin("styles.css"),
-
-    // sets the parameters of other loaders
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-
     // need this to remove the dev-only bundled js
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify(env)
       }
     }),
-
-    // minifies the code
-    new webpack.optimize.UglifyJsPlugin({
-      comments: false
-    })
   ]
 })
--- config/webpack.prod.babel.js
+++ config/webpack.prod.babel.js
@@ -1,6 +1,50 @@
 import base from './webpack.base.babel';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import merge from 'webpack-merge';
+import webpack from 'webpack';
 
 export default (env) => merge(base(env), {
+  entry: {
+    js: './index.js',
+    vendor: ['react', 'react-dom']
+  },
 
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: "style-loader",
+          loader: "css-loader"
+        }),
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    // assign modules and chunks shorter ids to lower file size
+    new webpack.optimize.OccurrenceOrderPlugin(),
+
+    // seperates out the vendor file into another chunk
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.bundle.js'
+    }),
+
+    // extracts CSS from JS and puts into external file
+    new ExtractTextPlugin("styles.css"),
+
+    // sets the parameters of other loaders
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+
+    // minifies the code
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false
+    })
+  ]
 })
