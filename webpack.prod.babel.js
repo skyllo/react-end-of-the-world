6f329a9389d08957322af06bf114b4faef2c11b7
skyllo <skyllo.dev@gmail.com>
2016-11-19 11:55:52 +0000

    SAVEPOINT

--- webpack.prod.babel.js
+++ webpack.prod.babel.js
@@ -3,74 +3,72 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 
-export default (env) => {
-	return {
-		entry: {
-			js: './index.js',
-			vendor: ['react', 'react-dom']
-		},
+export default (env) => ({
+  entry: {
+    js: './index.js',
+    vendor: ['react', 'react-dom']
+  },
 
-		output: {
-			filename: '[name].bundle.js',
-			path: resolve(__dirname, 'dist'),
-      publicPath: '/'
-		},
+  output: {
+    filename: '[name].bundle.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
 
-		context: resolve(__dirname, 'src'),
+  context: resolve(__dirname, 'src'),
 
-		module: {
-			rules: [
-        {
-					test: /\.js$/,
-					loaders: ['babel-loader'],
-					exclude: /node_modules/
-				},
-				{
-					test: /\.css$/,
-					loader: ExtractTextPlugin.extract({
-						fallbackLoader: "style-loader",
-						loader: "css-loader"
-					})
-				}
-			]
-		},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: "style-loader",
+          loader: "css-loader"
+        })
+      }
+    ]
+  },
 
-		plugins: [
-      // bundle HTML page
-      new HtmlWebpackPlugin({
-        template: 'index.html'
-      }),
+  plugins: [
+    // bundle HTML page
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
 
-			// assign modules and chunks shorter ids to lower file size
-			new webpack.optimize.OccurrenceOrderPlugin(),
+    // assign modules and chunks shorter ids to lower file size
+    new webpack.optimize.OccurrenceOrderPlugin(),
 
-			// seperates out the vendor file into another chunk
-			new webpack.optimize.CommonsChunkPlugin({
-				name: 'vendor',
-				minChunks: Infinity,
-				filename: 'vendor.bundle.js'
-			}),
+    // seperates out the vendor file into another chunk
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.bundle.js'
+    }),
 
-			// extracts CSS from JS and puts into external file
-			new ExtractTextPlugin("styles.css"),
+    // extracts CSS from JS and puts into external file
+    new ExtractTextPlugin("styles.css"),
 
-			// sets the parameters of other loaders
-			new webpack.LoaderOptionsPlugin({
-				minimize: true,
-				debug: false
-			}),
+    // sets the parameters of other loaders
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
 
-			// need this to remove the dev-only bundled js
-			new webpack.DefinePlugin({
-				'process.env': {
-					'NODE_ENV': JSON.stringify(env)
-				}
-			}),
+    // need this to remove the dev-only bundled js
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(env)
+      }
+    }),
 
-			// minifies the code
-			new webpack.optimize.UglifyJsPlugin({
-				comments: false
-			})
-		]
-	}
-};
+    // minifies the code
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false
+    })
+  ]
+})
