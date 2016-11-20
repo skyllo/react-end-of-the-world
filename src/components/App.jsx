fe50bb8713248f88f7d1f0105ae4a79ce044d94c
skyllo <skyllo.dev@gmail.com>
2016-11-20 16:00:05 +0000

    SAVEPOINT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import styles from './App.scss';
 
-export const App = () => (
-  <div className={styles.app}>
-    <h2>Ho2a1,</h2>
-  </div>
-);
+const CURRENT_DATE = 4540000000;
+const END_DATE = CURRENT_DATE + 5000000000;
 
-export function fuckThis() {
-  console.log('fuck!!!!!');
+export class App extends React.Component {
+  render() {
+    const progress = (END_DATE / CURRENT_DATE) * 100;
+    return (
+      <div className={styles.app}>
+        <div className={styles.app__lineContainer}>
+          <div className={styles.app__line} />
+          <div className={styles.app__lineProgress} style={{ width: progress }} />
+        </div>
+        <div className={styles.app__textContainer}>
+          <div className={styles.app__textStart}>Earth's Beginning</div>
+          <div className={styles.app__textCurrent} style={{ left: progress }}>Current Time</div>
+          <div className={styles.app__textEnd}>Earth's End</div>
+        </div>
+      </div>
+    )
+  }
 }
