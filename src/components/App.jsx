3e3ed532c31a266f6f84027545dc19ba69951e7e
skyllo <skyllo.dev@gmail.com>
2016-12-28 14:58:56 +0000

    SAVEPOINT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,21 @@ import styles from './App.scss';
 const CURRENT_DATE = 4540000000;
 const END_DATE = CURRENT_DATE + 5000000000;
 
-export class App extends React.Component {
-  render() {
-    const progress = (END_DATE / CURRENT_DATE) * 100;
-    return (
-      <div className={styles.app}>
-        <div className={styles.app__lineContainer}>
-          <div className={styles.app__line} />
-          <div className={styles.app__lineProgress} style={{ width: progress }} />
-        </div>
-        <div className={styles.app__textContainer}>
-          <div className={styles.app__textStart}>Earth's Beginning</div>
-          <div className={styles.app__textCurrent} style={{ left: progress }}>Current Time</div>
-          <div className={styles.app__textEnd}>Earth's End</div>
-        </div>
+const App = () => {
+  const progress = (END_DATE / CURRENT_DATE) * 100;
+  return (
+    <div className={styles.app}>
+      <div className={styles.app__lineContainer}>
+        <div className={styles.app__line} />
+        <div className={styles.app__lineProgress} style={{ width: progress }} />
       </div>
-    )
-  }
-}
+      <div className={styles.app__textContainer}>
+        <div className={styles.app__textStart}>Earth's Beginning</div>
+        <div className={styles.app__textCurrent} style={{ left: progress }}>Current Time</div>
+        <div className={styles.app__textEnd}>Earth's End</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
